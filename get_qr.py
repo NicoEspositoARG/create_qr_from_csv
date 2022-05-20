5d9aa545f069deba1aa719c26527a36ef647a22c
Nico Esposito <nico.esposito@gmail.com>
2022-05-20 18:26:59 +0000

    Some refactor and renaming.

--- get_qr.py
+++ get_qr.py
@@ -2,10 +2,10 @@
     and create a resultant csv file with file name."""
 # https://realpython.com/python-csv/
 # https://towardsdatascience.com/generate-qrcode-with-python-in-5-lines-42eda283f325
+
 import csv
 import logging
 import sys
-
 import qrcode
 from requests.utils import requote_uri
 
@@ -27,14 +27,9 @@ def _create_file_name(raw_url):
     return vendor_name[:end_index]
 
 
-def process_csv_row(row, line):
-    raw_url = str(row[0])
-    _.info(f"Raw URL: {raw_url}")
-    parsed_url = requote_uri(raw_url)
-    _.info(f"Parsed URL: {parsed_url}")
-    vendor_name = _create_file_name(raw_url)
-    qr_file_name = f'images/{line}-{vendor_name}.png'
-    # Creating an instance of qrcode
+def _create_qr_code(parsed_url, qr_file_name):
+    """Creating an instance of qrcode"""
+
     qr = qrcode.QRCode(
         version=1,
         box_size=10,
@@ -43,32 +38,44 @@ def process_csv_row(row, line):
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
     img.save(qr_file_name)
+
+
+def process_csv_row(row, line):
+    raw_url = str(row[0])
+    _.info(f"Raw URL: {raw_url}")
+    parsed_url = requote_uri(raw_url)
+    _.info(f"Parsed URL: {parsed_url}")
+    vendor_name = _create_file_name(raw_url)
+    qr_file_name = f'images/{line}-{vendor_name}.png'
+    _create_qr_code(parsed_url, qr_file_name)
     file_result_name = qr_file_name.split('images/')[1]
-    return file_result_name
+    return (parsed_url, file_result_name)
 
 
-def process():
+def process_file():
     with open('Vendedores TV.csv', mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_reader = csv.reader(csv_file, delimiter=';')
         line_count = 0
+        output_row = []
         with open('results.csv', mode='w') as result_file:
-            result_writer = csv.writer(
+            results_writer = csv.writer(
                 result_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for row in csv_reader:
                 if line_count == 0:
                     _.info(f'Column names are {", ".join(row)}')
-                    result_writer.writerow(row)
-                    line_count += 1
+                    output_row = row
                 else:
-                    qr_file_name = process_csv_row(row, line_count)
-                    result_writer.writerow([row[0], qr_file_name])
+                    parsed_url, file_result_name = process_csv_row(
+                        row, line_count)
+                    output_row = [parsed_url, file_result_name]
 
-                    line_count += 1
-                    # if line_count == 3:
-                    #     break
+                # if line_count == 3:
+                #     break
+                line_count += 1
+                results_writer.writerow(output_row)
 
         _.info(f"TOTAL: {line_count}")
 
 
 if __name__ == "__main__":
-    process()
+    process_file()
